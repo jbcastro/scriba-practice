0e473febc3b68692f92060031f7f7590d3e072a1
jbcastro <josephbeckcastro@gmail.com>
2019-02-21 18:14:57 -0800

    2:20 in the video, getting rid of commented stuff on App and others

--- src/App.js
+++ src/App.js
@@ -3,16 +3,87 @@ import TodoItem from "./TodoItem"
 import TodosData from "./TodosData"
 
 
+
+// Given a stateless functional component, add state to it
+// state should have a property called `isLoggedIn` which is a boolean
+// (true if logged in, false if not)
+// Then, give your best shot at rendering the word "in" if the user is logged in
+// or "out" if the user is logged out.
+
 class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      isLoggenIn: false
+    }
+  }
   render() {
-    const todoItems = TodosData.map(item => (
-      <TodoItem key={item.id} todo={item} />
-    ))
     return (
-      <div>{todoItems}</div>
+      <div>
+        <h1>You are currently logged {this.state.isLoggenIn ? 'IN' : 'OUT'}</h1>
+      </div>
     )
   }
 }
+
+
+
+// class App extends React.Component {
+//   constructor(props) {
+//     super(props)
+//     this.state = {
+//       name: "Steve",
+//       age: "55",
+//       butt: "Large"
+//     }
+
+//     this.handleClick = this.handleClick.bind(this);
+//   }
+
+//   handleClick() {
+//     const ass = this.state.butt
+//     console.log({ ass })
+
+//   }
+//   render() {
+//     return (
+//       <div>
+//         <h1>{this.state.name}</h1>
+//         <h3>{this.state.age} years old</h3>
+//         <button onClick={this.handleClick}>Push</button>
+//         <h1>{this.state.butt}</h1>
+//       </div>
+//     )
+//   }
+// }
+
+
+// class App extends React.Component {
+//   constructor() {
+//     super()
+//     this.state = {
+//       answer: "Yes"
+//     }
+//   }
+//   render() {
+//     return (
+//       <div>
+//         <h1>Is state important to know? {this.state.answer}</h1>
+//       </div>
+//     )
+//   }
+// }
+
+// class App extends React.Component {
+//   render() {
+//     const todoItems = TodosData.map(item => (
+//       <TodoItem key={item.id} todo={item} />
+//     ))
+//     return (
+//       <div>{todoItems}</div>
+//     )
+//   }
+// }
 
 
 // class App extends React.Component {
--- src/index.js
+++ src/index.js
@@ -4,3 +4,60 @@ import "./index.css";
 import App from "./App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+
+
+
+
+
+
+// import React, { Component } from "react"
+// import ReactDOM from "react-dom"
+
+// // #1
+// class App extends React.Component {
+//     render() {
+//         return (
+//             <div>
+//                 <Header username="Steve" />
+//                 <Greeting />
+//             </div>
+//         )
+//     }
+// }
+
+// // #2
+// class Header extends React.Component {
+//     render(props) {
+//         return (
+//             <header>
+//                 <p>Welcome, {this.props.username}!</p>
+//             </header>
+//         )
+//     }
+// }
+
+// // #3
+// class Greeting extends React.Component {
+//     render() {
+//         const date = new Date()
+//         const hours = date.getHours()
+//         let timeOfDay
+
+//         if (hours < 12) {
+//             timeOfDay = "morning"
+//         } else if (hours >= 12 && hours < 17) {
+//             timeOfDay = "afternoon"
+//         } else {
+//             timeOfDay = "night"
+//         }
+//         return (
+//             <h1>Good {timeOfDay} to you, sir or madam!</h1>
+//         )
+//     }
+// }
+
+// ReactDOM.render(<App />, document.getElementById("root"))
+
+
+
